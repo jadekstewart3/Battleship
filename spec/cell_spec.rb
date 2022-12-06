adb8af91d5ec80e0a9d208f1af5b9cc5ef4528ca
Chris Crane <chrisjcrane@gmail.com>
2022-12-05 20:03:53 -0700

    Fix title of exist test

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -3,7 +3,7 @@ require './lib/ship'
 require './lib/cell'
 
 RSpec.describe Cell do 
-    it "it exists" do 
+    it "exists" do 
         cell = Cell.new("B4")
         expect(cell).to be_an_instance_of(Cell)
     end
