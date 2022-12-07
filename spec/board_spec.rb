91f4d861b6b175d616d4817ae753c99f036fb217
Chris Crane <chrisjcrane@gmail.com>
2022-12-07 15:20:46 -0700

    Add test for valid coordinates

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -15,4 +15,12 @@ RSpec.describe Board do
         expect(@board.cells).to be_a(Hash)
         expect(@board.cells.length).to eq(16)
     end
+
+    it "confirms valid coordinates" do
+        expect(board.valid_coordinate?("A1")).to eq(true)
+        expect(board.valid_coordinate?("D4")).to eq(true)
+        expect(board.valid_coordinate?("A5")).to eq(false)
+        expect(board.valid_coordinate?("E1")).to eq(false)
+        expect(board.valid_coordinate?("A22")).to eq(false)
+    end
 end
