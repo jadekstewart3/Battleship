c8f6dab88ffaaf56a719555eb580e20ab1c383e6
Christopher Crane <116330317+GreenGogh47@users.noreply.github.com>
2022-12-09 15:38:32 -0700

    Revert "A place ship method that will not overlap ships"

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -76,13 +76,13 @@ RSpec.describe Board do
         expect(cell_3.ship == cell_2.ship).to be(true)
     end
 
-    it "can not have overlapping ships" do
+    xit "can not have overlapping ships" do
+        board = Board.new
+        
         cruiser = Ship.new("Cruiser", 3)
-        @board.place(cruiser, ["A1", "A2", "A3"])
-
+        board.place(cruiser, ["A1", "A2", "A3"])
         
         submarine = Ship.new("Submarine", 2)
-        # require "pry"; binding.pry
-        expect(@board.valid_placement?(submarine, ["A1", "B1"])).to eq(false)
+        board.valid_placement?(submarine, ["A1", "B1"])
     end
 end
