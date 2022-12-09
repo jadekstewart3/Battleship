eca0d827b89f76879d62b03dd656415077f91997
Jade Stewart <jadekstewart3@gmail.com>
2022-12-09 13:20:38 -0700

    Break Valid_placement method into helper methods, write place method, write and pass tests for helper methods, write and pass place ships test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -32,35 +32,44 @@ RSpec.describe Board do
         expect(@board.valid_placement?(submarine, ["A2", "A3", "A4"])).to be(false)
     end
 
-    it "accepts valid coordinates" do
+
+    it "verifies valid length of ship to coordinates" do
         cruiser = Ship.new("Cruiser", 3)
         submarine = Ship.new("Submarine", 2)
-
-        expect(@board.valid_placement?(cruiser, ["A1", "A2", "A3"])).to be(true)
-        # expect(@board.valid_placement?(submarine, ["A2", "A3"])).to be(true)
-
+        expect(@board.valid_length?(submarine, ["A1", "A2"])).to be(true)
+        expect(@board.valid_length?(submarine, ["A1", "A2", "A3"])).to be(false)
     end
 
-    it "ship coordinates are concecutive" do
+    it "ship coordinates are consecutive" do
         cruiser = Ship.new("Cruiser", 3)
         submarine = Ship.new("Submarine", 2)
         
         # require "pry"; binding.pry
 
-        expect(@board.valid_placement?(submarine, ["A1", "A2"])).to be(true)
-        expect(@board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to be(false)
-        expect(@board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to be(false)
-        expect(@board.valid_placement?(submarine, ["A1", "C1"])).to be(false)
-        expect(@board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to be(false)
-        expect(@board.valid_placement?(submarine, ["C1", "B1"])).to be(false)
-    end        
+        expect(@board.consecutive_coordinates?(["A1", "A2"])).to be(true)
+        expect(@board.consecutive_coordinates?(["A1", "B2", "C3"])).to be(false)
+        expect(@board.consecutive_coordinates?(["A1", "A2", "A4"])).to be(false)
+        expect(@board.consecutive_coordinates?(["A1", "C1"])).to be(false)
+        expect(@board.consecutive_coordinates?(["A3", "A2", "A1"])).to be(false)
+        expect(@board.consecutive_coordinates?(["C1", "B1"])).to be(false)
+    end     
+
+    it "accepts valid coordinates" do
+        cruiser = Ship.new("Cruiser", 3)
+        submarine = Ship.new("Submarine", 2)
+
+        expect(@board.valid_placement?(cruiser, ["A1", "A2", "A3"])).to be(true)
+        expect(@board.valid_placement?(submarine, ["A2", "A3"])).to be(true)
+        expect(@board.valid_placement?(submarine, ["A2", "A4"])).to be(false)
+
+    end
 
     it "can place ships" do 
         cruiser = Ship.new("Cruiser", 3)
-        board.place(cruiser, ["A1", "A2", "A3"])
-        cell_1 = board.cells["A1"]
-        cell_2 = board.cells["A2"]
-        cell_3 = board.cells["A3"]
+        @board.place(cruiser, ["A1", "A2", "A3"])
+        cell_1 = @board.cells["A1"]
+        cell_2 = @board.cells["A2"]
+        cell_3 = @board.cells["A3"]
         expect(cell_1.ship).to eq(cruiser)
         expect(cell_2.ship).to eq(cruiser)
         expect(cell_3.ship).to eq(cruiser)
