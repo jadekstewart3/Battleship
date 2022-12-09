02b5b0b7dd3aa57b926f2152e25b07ed2031496a
Chris Crane <chrisjcrane@gmail.com>
2022-12-09 14:06:44 -0700

    changed the order of tests after conflict resolution through github

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -72,17 +72,6 @@ RSpec.describe Board do
       
     end
 
-    xit "can not have overlapping ships" do
-        board = Board.new
-        
-        cruiser = Ship.new("Cruiser", 3)
-        board.place(cruiser, ["A1", "A2", "A3"])
-        
-        submarine = Ship.new("Submarine", 2)
-        board.valid_placement?(submarine, ["A1", "B1"])
-
-    end
-
     it "can place ships" do 
         cruiser = Ship.new("Cruiser", 3)
         @board.place(cruiser, ["A1", "A2", "A3"])
@@ -93,5 +82,15 @@ RSpec.describe Board do
         expect(cell_2.ship).to eq(cruiser)
         expect(cell_3.ship).to eq(cruiser)
         expect(cell_3.ship == cell_2.ship).to be(true)
+    end
+
+    xit "can not have overlapping ships" do
+        board = Board.new
+        
+        cruiser = Ship.new("Cruiser", 3)
+        board.place(cruiser, ["A1", "A2", "A3"])
+        
+        submarine = Ship.new("Submarine", 2)
+        board.valid_placement?(submarine, ["A1", "B1"])
     end
 end
