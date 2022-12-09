dce9a31070dcd929a70ab44a61c0f289e440b9e5
Chris Crane <chrisjcrane@gmail.com>
2022-12-08 21:34:41 -0700

    created tests for overlapping_ships method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -56,6 +56,16 @@ RSpec.describe Board do
         expect(@board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to be(false)
         expect(@board.valid_placement?(submarine, ["C1", "B1"])).to be(false)
         
-    end        
+    end
+
+    xit "can not have overlapping ships" do
+        board = Board.new
+        
+        cruiser = Ship.new("Cruiser", 3)
+        board.place(cruiser, ["A1", "A2", "A3"])
+        
+        submarine = Ship.new("Submarine", 2)
+        board.valid_placement?(submarine, ["A1", "B1"])
+    end
 
 end
