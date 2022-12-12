ed8a59b71b9c84ba6b674b41f72a722320841ee1
Jade Stewart <jadekstewart3@gmail.com>
2022-12-12 15:28:57 -0700

    Create helper methods for turn method

--- lib/game.rb
+++ lib/game.rb
@@ -83,48 +83,70 @@ class Game
         puts   ">"
 
         player_input_cruiser
-
-        puts "Enter the squares for the Submarine (2 spaces):\n>"
-
+            puts "Enter the squares for the Submarine (2 spaces):\n>"
         player_input_sub
-
-        puts "=============COMPUTER BOARD============="
-        comp_board.render(true) #remember to make this not true
-        puts "==============PLAYER BOARD=============="
-        player_board.render(true)
-
-        puts "Enter the coordinate for your shot:"
-        
-        
-        
-        puts "Enter the coordinate for your shot: \n>"
+        turn
+    end  
+    
+    def player_fire
         player_shot = gets.chomp.to_s.upcase
+        if @comp_board.valid_coordinate?(player_shot) == false
+            puts "That isn't even in the ocean. Aim better." 
+            player_fire
+        elsif @comp_board.cells[player_shot].fired_upon? == true
+            puts "You've already fired there. Try to keep up, human."
+            player_fire
+        elsif @comp_board.cells[player_shot].fire_upon
+            if (@comp_board.cells[player_shot].fired_upon? == false) && @comp_board.cells[player_shot].empty?
+                puts "Your shot on #{player_shot} has missed!! Huzzah!!"
+            elsif (@comp_board.cells[player_shot].ship.sunk? == false) && (@comp_board.cells[player_shot].empty? == false)
+                puts "You've managed to land a hit on #{player_shot}! It seems I've underestimated you...."
+            else @comp_board.cells[player_shot].ship.sunk?
+                puts "You've sunk my #{@comp_board.cells[player_shot].ship.name}!!!!! Damn youuuuuuuuuu!!!!!"
+            end
+        end
+    end
+
+    def comp_fire
+        comp_shot = @player_board.cells.keys.sample
+        until (@player_board.valid_coordinate?(comp_shot) == true) && (@player_board.cells[comp_shot].fired_upon? == false)
+            @player_board.cells[comp_shot].fire_upon
+        end
+    end
         
-        valid_attack = comp_board.cells.keys.include?(player_shot) && (comp_board.cells[player_shot].fired_upon == false)
-        require "pry"; binding.pry
-        # if player_shot == valid_attack
-        # comp_board.cells["A1"].fire_upon
-        # player_board.cells.keys.sample[#call random not fired on cell].fire_upon
-        
+    def turn
+        until (player_win? == true) || (comp_win? == true)
+            puts "=============COMPUTER BOARD============="
+                comp_board.render 
+            puts "==============PLAYER BOARD=============="
+                player_board.render(true)
+            puts "Enter the coordinate for your shot:"
+            player_fire
+            comp_fire
+        end
+        # ships.each do |ship|
+        # coordinates = valid_comp_coordinates(ship)
+        # @comp_board.place(ship, coordinates)
+        #goal: comp selects a random cell 
+        #then: we determine if target is valid
+        #if not valid- pick new cell
+        #esif is vaild - fire
+    end
+    
+    # Player choosing a coordinate to fire on
+    # Computer choosing a coordinate to fire on
+    # Reporting the result of the Player’s shot
+    # Reporting the result of the Computer’s shot
+    
+    def player_win?
+        if (@comp_cruiser.sunk? == true) && (@comp_sub.sunk? == true)
+            puts "YOU WIN?!?!? NOOOOOOoooooooooooo........ *gurgle* *gurgle*"
+        end
+    end
 
-
-
-
-        
-
-        # game_over = player_cruiser.sunk? && player_sub.sunk? #stop condition for the loop
-        # until game_over
-            # puts "=============COMPUTER BOARD============="
-            # comp_board.render
-            # puts "==============PLAYER BOARD=============="
-            # player_board.render
-
-        # Player choosing a coordinate to fire on
-        # Computer choosing a coordinate to fire on
-        # Reporting the result of the Player’s shot
-        # Reporting the result of the Computer’s shot
-        
-
-
+    def comp_win?
+        if (@player_cruiser.sunk? == true) && (@player_sub.sunk? == true)
+            puts "Sleep with the fishes, human."
+        end
     end
 end
